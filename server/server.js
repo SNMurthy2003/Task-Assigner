c12a067e2b3ba8b772954d411f1b046c15958892
SNMurthy2003 <chebolu.snmurthy@gmail.com>
2026-02-08 00:56:35 +0530

    Add debug endpoint to check env vars

--- server/server.js
+++ server/server.js
@@ -17,6 +17,16 @@ app.get('/', (req, res) => {
   res.json({ message: 'Task Manager API is running' });
 });
 
+app.get('/api/debug', (req, res) => {
+  res.json({
+    hasJwtSecret: !!process.env.JWT_SECRET,
+    hasProjectId: !!process.env.FIREBASE_PROJECT_ID,
+    hasClientEmail: !!process.env.FIREBASE_CLIENT_EMAIL,
+    hasPrivateKey: !!process.env.FIREBASE_PRIVATE_KEY,
+    privateKeyStart: process.env.FIREBASE_PRIVATE_KEY?.substring(0, 30) || 'NOT SET',
+  });
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
